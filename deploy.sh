856a18af921582b05877ce7db7f273a623644286
YumeParin <antoinetorrez2006@gmail.com>
2025-12-04 16:30:18 +0100

    Doc: Update

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,18 @@ set -euo pipefail
 # # -v flag is to take down the volumes
 docker-compose down -v
 
-sudo pkill -f "node src/index.js" || true
-sudo pkill -f "npm start" || true
-
 echo "Building new Docker images..."
 docker-compose build --no-cache --pull
 
 echo "Launching de Shrine the Melodies..."
 docker-compose up -d 
 
-echo "Cleaning up dangling images..."d
+echo "Cleaning up dangling images..."
 docker image prune -f
 
 HOSTNAMEIP="$(hostname -I | awk '{print $1}')"
 echo "RaikoMusics is now operational:"
 echo "  - Frontend is available at http://$HOSTNAMEIP:6777 and http://localhost:6777"
 echo "  - Stream Server is available at http://$HOSTNAMEIP:6888 and http://localhost:6888"
+echo "  - API Server is available at http://$HOSTNAMEIP:6999 and http://localhost:6999"
 
